e4647cd41df92a4b1be05e515fd49fddfbbc3ed4
Wasid Hossain <wasidmail@gmail.com>
2016-10-04 12:32:04 +0000

    done till profile update

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+@include('includes.message')
     <section class="row new-post">
         <div class="col-md-6 col-md-offset-3">
             <header><h3>Your Account</h3></header>
